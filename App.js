53bc19e011f9b80d03b9bea2c13f00607279f798
YogiHa <hagaiharari1@gmail.com>
2019-04-15 18:39:41 +0200

    fixing default display on research

--- App.js
+++ App.js
@@ -45,7 +45,7 @@ return()=> {isCelzios= true}
 function Forcast({location}){
 	let ignore = false;
 	const [data, setData] = useState([])
-	const apiKey = 'd2cb9ea67b28bb32949f13c1ffb2b803';
+	const apiKey = //APIkey;
 	useEffect(()=> {
 		async function fetchData() {
       const result = await axios(`http://api.openweathermap.org/data/2.5/weather?q=${location.text}&units=metric&appid=${apiKey}`)
