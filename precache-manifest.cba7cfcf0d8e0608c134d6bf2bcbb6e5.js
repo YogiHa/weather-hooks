cb1140d8a10d44ae827759dcf0f23f504b40b080
Hagai <hagaiharari1@gmail.com>
2019-05-22 18:32:30 +0300

    Updates

---
+++ precache-manifest.cba7cfcf0d8e0608c134d6bf2bcbb6e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90a1de53869328a5854f878f58563847",
+    "url": "/weather-hooks/index.html"
+  },
+  {
+    "revision": "39c68b4bc55db3808e98",
+    "url": "/weather-hooks/static/css/main.7a990bfb.chunk.css"
+  },
+  {
+    "revision": "6632bf8b3971f2e2a348",
+    "url": "/weather-hooks/static/js/2.cf29e8fd.chunk.js"
+  },
+  {
+    "revision": "39c68b4bc55db3808e98",
+    "url": "/weather-hooks/static/js/main.b74f77b9.chunk.js"
+  },
+  {
+    "revision": "d57ab0865ea799005a4d",
+    "url": "/weather-hooks/static/js/runtime~main.39c08578.js"
+  },
+  {
+    "revision": "c8cc95c6a11f9ba4a9100f88be5835bf",
+    "url": "/weather-hooks/static/media/bg.c8cc95c6.jpg"
+  }
+]);
